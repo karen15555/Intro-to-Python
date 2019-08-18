b25e0fdd12aa2787a02e6cce8e8cf594f13410cd
Frankfurt <Karen@Karens-MacBook-Air.local>
2019-08-18 13:52:52 +0400

    final commit

---
+++ Lecture 1/Practical1/Problem3.py
@@ -0,0 +1,11 @@
+course = "Intro to Python"
+student = "Karen Karakhanyan"
+nickname = "Frankfurt"
+grade = 1
+pythonExpert = True
+
+print(course)
+print(student)
+print(nickname)
+print(grade)
+print(pythonExpert)
---
+++ Lecture 1/Practical1/Problem5.py
@@ -0,0 +1,10 @@
+AB = 3
+AC = 4
+import math
+BC = math.sqrt(AB ** 2 + AC ** 2)
+
+
+
+print(AB)
+print(AC)
+print(BC)
