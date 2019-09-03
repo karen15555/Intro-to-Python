59169d967ab9d2837913b5ec4ae502169002b785
Frankfurt <Karen@Karens-MacBook-Air.local>
2019-09-04 00:13:25 +0400

    Final commit

--- Lecture2/Practical2/problem10.py
+++ Lecture2/Practical2/problem10.py
@@ -5,6 +5,8 @@ import calendar
 
 localtime = time.localtime()
 print ("Local current time :", localtime)
+tnow = datetime.datetime.now().time()
+print(tnow)
 
 tday = datetime.date.today()
 print('Date: ', tday)
@@ -12,4 +14,13 @@ print('Year: ', tday.year)
 print('Month: ', tday.month)
 print('Week: ', tday.isoweekday())
 
+tday = datetime.date.today()
+print('Date today: ', tday)
+tdelta1 = datetime.timedelta(days = 5)
+print(tdelta1)
+print('Date today - 5 days: ', tday - tdelta1)
 
+tdelta2 = datetime.timedelta(seconds = 5)
+print(tdelta2)
+
+print('Time now + 5 seconds: ', tnow + tdelta2)
